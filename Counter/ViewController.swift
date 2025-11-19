d0fee042798201cecf9ae75ef3f0a04695837919
shemitov.ol <shemitov.ol@yandex.com>
2025-11-20 00:16:07 +0500

    Создан проект

---
+++ Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Олег Сергеевич on 20.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
