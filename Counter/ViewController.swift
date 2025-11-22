2f7575ea11657ee7b9cb74274fae7fe05051e2a9
shemitov.ol <shemitov.ol@yandex.com>
2025-11-22 17:12:33 +0500

    Сделана усложненная задача

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -10,18 +10,56 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var counterValue: UILabel!
-    
+    @IBOutlet weak var updateBotton: UIButton!
     @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var historyOfChanges: UITextView!
     
-    var value: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-    @IBAction func increaseCounter(_ sender: Any) {
-        value += 1
+    
+    private var value: Int = 0
+    
+    private func dateFormatter() -> String {
+        let dateFormatter = DateFormatter ()
+        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        let dateNow = dateFormatter.string(from: Date())
+        return dateNow
+    }
+    
+    private func changeCounterText() {
         counterValue.text = "Значение счетчика: \(value)"
+    }
+    
+    private func historyChange(_ change: String) {
+        historyOfChanges.text.append("\n \(dateFormatter()): \(change)")
+    }
+    
+    @IBAction private func increaseCounter(_ sender: Any) {
+        if value == Int.max {
+            historyChange("попытка увеличить максимальное значение счетчика")
+        } else {
+            value += 1
+            changeCounterText()
+            historyChange("значение изменено на +1")
+        }
+    }
+    
+    @IBAction private func reduceCounter(_ sender: Any) {
+        if value > 0 {
+            value -= 1
+            changeCounterText()
+            historyChange("начение изменено на -1")
+        } else {
+            historyChange("попытка уменьшить значение счётчика ниже 0")
+        }
+    }
+    
+    @IBAction private func updateCounterValue(_ sender: Any) {
+        value = 0
+        changeCounterText()
+        historyChange("значение сброшено")
     }
     
 }
